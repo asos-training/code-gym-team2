df9e8ed07f1038988318754b9bc6d1795102abed
Luke Finn <Luke.Finn@asos.com>
2025-02-05 21:29:28 +0000

    Continued the kata

--- quickstart/dotnet-code-gym/CodeGym/Kata.cs
+++ quickstart/dotnet-code-gym/CodeGym/Kata.cs
@@ -5,6 +5,12 @@ namespace CodeGym;
 public class Game
 {
     public List<Player> Players { get; set; }
+    private Dictionary<char, int> shipLengths = new Dictionary<char, int>()
+    {
+        {'c', 4},
+        {'d', 3},
+        {'g', 1}
+    };
     public Game()
     {
         Players = new List<Player>();
@@ -22,6 +28,34 @@ public class Game
         Players.Add(player);
 
         return $"Added Player {player.Name}";
+    }
+
+    public Player GetPlayer(string player1)
+    {
+        return Players.First(x => x.Name == player1);
+    }
+
+    public void Start(string player, char ship, int row, int column, bool isVertical = false)
+    {
+        var playerObject = GetPlayer(player);
+
+        if (isVertical == true)
+        {
+            var thisShpLength = shipLengths[ship];
+            for (int i = row; i <= row + thisShpLength; i++)
+            {
+                playerObject.Board[i, column] = ship;
+            }
+        }
+        else
+        {
+            var thisShpLength = shipLengths[ship];
+            for (int i = column; i <= column + thisShpLength; i++)
+            {
+                playerObject.Board[row, i] = ship;
+            }
+        }
+        
     }
 }
 
--- quickstart/dotnet-code-gym/CodeGymTests/KataTests.cs
+++ quickstart/dotnet-code-gym/CodeGymTests/KataTests.cs
@@ -33,4 +33,35 @@ public class AGameShould
         Assert.That(() => game.AddPlayer("Player3"), Throws.TypeOf<ArgumentOutOfRangeException>().With.Message.EqualTo("Specified argument was out of the range of valid values."));
     }
 
+    [Test]
+    public void StartAGameWithOneSingleSpaceShip()
+    {
+        var game = new Game();
+        game.AddPlayer("Player1");
+        game.Start("Player1", 'g', 0, 0);
+        Assert.That(game.GetPlayer("Player1").Board[0,0], Is.EqualTo('g'));
     }
+
+    [Test]
+    public void StartTheGameWithOneMultiVerticalSpaceShip()
+    {
+        var game = new Game();
+        game.AddPlayer("Player1");
+        game.Start("Player1", 'd', 0, 0, true);
+        Assert.That(game.GetPlayer("Player1").Board[0, 0], Is.EqualTo('d'));
+        Assert.That(game.GetPlayer("Player1").Board[1, 0], Is.EqualTo('d'));
+        Assert.That(game.GetPlayer("Player1").Board[2, 0], Is.EqualTo('d'));
+    }
+
+    [Test]
+    public void StartTheGameWithOneMultiHorizontalSpaceShip()
+    {
+        var game = new Game();
+        game.AddPlayer("Player1");
+        game.Start("Player1", 'd', 1, 0, false);
+        Assert.That(game.GetPlayer("Player1").Board[1, 0], Is.EqualTo('d'));
+        Assert.That(game.GetPlayer("Player1").Board[1, 1], Is.EqualTo('d'));
+        Assert.That(game.GetPlayer("Player1").Board[1, 2], Is.EqualTo('d'));
+    }
+
+}
